4a056190dfcff078839277d9661d36016c9fa9ae
Carolis <89231697+carolisc@users.noreply.github.com>
2023-12-29 01:55:27 -0300

    creating and optimizing every input

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+function calculateCalories(e) {
+
+}
+
 function getCaloriesFromInputs(list) {
   let calories = 0;
 
@@ -39,8 +43,14 @@ function getCaloriesFromInputs(list) {
     const currVal = cleanInputString(list[i].value);
     const invalidInputMatch = isInvalidInput(currVal);
 
-    
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
   }
+  return calories;
 }
 
 addEntryButton.addEventListener("click", addEntry);
